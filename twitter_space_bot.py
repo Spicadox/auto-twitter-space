b2dc95d342860d5f994faafd7c7bda9c3da78910
Sam Phung <sphung@uncc.edu>
2021-12-29 17:57:06 -0500

    Moved twitter_id list to const.py

--- twitter_space_bot.py
+++ twitter_space_bot.py
@@ -26,74 +26,15 @@ api = tweepy.API(auth)
 twitter_client = tweepy.Client(bearer_token, consumer_key=api_key, consumer_secret=api_key_secret,
                                access_token=access_token, access_token_secret=access_token_secret)
 
-# Gamers IDs
-discord_ids = [{"inugamikorone": 1109748792721432577}, {"nekomataokayu": 1109751762733301760},
-               {"ookamimio": 1063337246231687169}, {"shirakamifubuki": 997786053124616192}]
-# Gen 0 IDs
-discord_ids += [{'tokino_sora': 880317891249188864}, {'robocosan': 960340787782299648},
-                {'sakuramiko35': 979891380616019968},
-                {'suisei_hosimati': 975275878673408001}, {'AZKi_VDiVA': 1062499145267605504}]
-# Gen 1 IDs
-discord_ids += [{'yozoramel': 985703615758123008}, {'akirosenthal': 996643748862836736},
-                {'akaihaato': 998336069992001537}, {'natsuiromatsuri': 996645451045617664}]
-# Gen 2 IDs
-discord_ids += [{'minatoaqua': 1024528894940987392}, {'murasakishionch': 1024533638879166464},
-                {'nakiriayame': 1024532356554608640},
-                {'yuzukichococh': 1024970912859189248}, {'oozorasubaru': 1027853566780698624}]
-# Gen 3 IDs
-discord_ids += [{'houshoumarine': 1153192638645821440}, {'shiroganenoel': 1153195295573856256},
-                {'shiranuiflare': 1154304634569150464},
-                {'uruharushia': 1142975277175205888}, {'usadapekora': 1133215093246664706}]
-# Gen 4 IDs
-discord_ids += [{'himemoriluna': 1200396798281445376}, {'amanekanatach': 1200396304360206337},
-                {'tokoyamitowa': 1200357161747939328}, {'tsunomakiwatame': 1200397643479805957}]
-# Gen 5 IDs
-discord_ids += [{'omarupolka': 1270551806993547265}, {'yukihanalamy': 1255013740799356929},
-                {'shishirobotan': 1255015814979186689}, {'momosuzunene': 1255017971363090432}]
-# Gen 6 IDs
-discord_ids += [{'LaplusDarknesss': 1433657158067896325}, {'takanelui': 1433660866063339527},
-                {'hakuikoyori': 1433667543806267393},
-                {'sakamatachloe': 1433667543806267393}, {'kazamairohach': 1434755250049589252}]
-# Other IDs
-discord_ids += [{'ksononair': 733990222787018753}, {'tanigox': 2006101}, {'achan_UGA': 1064352899705143297},
-                {'daidoushinove': 1156797715319160832}, {"kotone": 986871577890312192}]
-
-# Holostars Gen 1 IDs
-discord_ids += [{'miyabihanasaki': 1132832428353966081}, {'arurandeisu': 1156841498479955968},
-                {'rikkaroid': 1174223248655114246}, {'kanadeizuru': 1132924263441227776}]
-# Holostars Gen 2 SunTempo IDs
-discord_ids += [{'kishidotemma': 1194519616472543232}, {'astelleda': 1181889913517572096},
-                {'yukokuroberu': 1194520283446530051}]
-# Holostars Gen 3 MaFia IDs
-discord_ids += [{'kageyamashien': 1248565757207695361}, {'aragamioga': 1248567107173773313}]
-
-# HoloID Gen 1 IDs
-discord_ids += [{'ayunda_risu': 1234752200145899520}, {'moonahoshinova': 1234753886520393729},
-                {'airaniiofifteen': 1235180878449397764}]
-
-# HoloID Gen 2 IDs
-discord_ids += [{'anyamelfissa': 1328277750000492545}, {'kureijiollie': 1328277233492844544},
-                {'pavoliareine': 1328275136575799297}]
-
-# HoloEN Gen 1 Myth IDs
-discord_ids += [{'moricalliope': 1283653858510598144}, {'takanashikiara': 1283646922406760448},
-                {'ninomaeinanis': 1283650008835743744}]
-discord_ids += [{'gawrgura': 1283657064410017793}, {'watsonameliaEN': 1283656034305769472}]
-
-# HoloEN Project Hope ID
-discord_ids += [{'irys_en': 1363705980261855232}]
-
-# HoloEN Gen 2 Council IDs
-discord_ids += [{'tsukumosana': 1409819816194576394}, {'ceresfauna': 1409784760805650436},
-                {'ourokronii': 1409817096523968513}]
-discord_ids += [{'nanashimumei_en': 1409817941705515015}, {'hakosbaelz': 1409783149211443200}]
+# List of twitter creators to monitor
+twitter_ids = const.twitter_ids
 
 space_fields = ['id', 'state', 'title', 'started_at']
 user_fields = ['profile_image_url']
 expansions = ['creator_id', 'host_ids']
 
 twitter_id_list = []
-for twitter_user in discord_ids:
+for twitter_user in twitter_ids:
     twitter_id_list.append(str(*twitter_user.values()))
 
 user_ids = ",".join(twitter_id_list)
