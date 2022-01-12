d31ed429c1d6892f2fcb3b4fbadfb46d7dac9396
Sam Phung <sphung@uncc.edu>
2022-01-12 16:00:04 -0500

    Added a means to log the output and replacing all the print statements

---
+++ log.py
@@ -0,0 +1,63 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os.path
+import const
+
+
+# Filter subclass that does not allow the file logging of sleeping messages
+class NoParsingFilter(logging.Filter):
+    def filter(self, record):
+        return not record.getMessage().startswith('Sleeping')
+
+
+def create_logger(logfile_name):
+    # Check if log dir exist and if not create it
+    logging.handlers.TimedRotatingFileHandler
+    log_dir = os.getcwd()+"\\logs"
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+
+    # Get the logger object
+    logger = logging.getLogger(__name__)
+
+    # If logger has already been created then return it(for the imported modules)
+    if len(logger.handlers) != 0:
+        return logger
+
+    # Set logging level and log path
+    logger.setLevel(logging.DEBUG)
+    log_path = log_dir + "\\" + logfile_name
+
+    # Create a new log file everyday
+    handler = TimedRotatingFileHandler(log_path, when="midnight", interval=1)
+    formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)d] %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M')
+    handler.setFormatter(formatter)
+    handler.suffix = "%Y%m%d"   # file suffix to be changed
+    handler.addFilter(NoParsingFilter())
+
+    # logging.basicConfig(level=logging.INFO,
+    #                     format='%(asctime)s [%(filename)s:%(lineno)d] %(levelname)-5s %(message)s',
+    #                     datefmt='%Y-%m-%d %H:%M',
+    #                     filename=log_path)
+
+    # define a Handler which writes DEBUG messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    # set a format which is simpler for console use
+    console_formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(console_formatter)
+    # add the handlers to the root logger
+    logger.addHandler(console)
+    logger.addHandler(handler)
+
+    # If logging is not enabled then remove the root log handler but keep the stream handler
+    if not const.LOGGING:
+        logger = logging.getLogger()  # this gets the root logger
+        try:
+            lhStdout = logger.handlers[0]  # stdout is the only handler initially
+        except IndexError as ierror:
+            logger.error(ierror)
+            return logger
+        logger.removeHandler(lhStdout)
+    return logger
