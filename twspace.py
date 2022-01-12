178426f517ce7a3593a4afe33ffc7400f29685f8
Sam Phung <sphung@uncc.edu>
2022-01-12 15:57:31 -0500

    Replaced print statments with logger, allow downloading from the m3u8's specified periscope server rather than just ap-northeast-1, and tried catching http 404 error and resending the request

--- twspace.py
+++ twspace.py
@@ -1,10 +1,12 @@
 import os
 import urllib.request
+from urllib import error
 import re
 import subprocess
 import const
 import discord
 import shutil
+from log import create_logger
 
 
 # Function takes in the file name and check if it contains illegal characters
@@ -23,57 +25,75 @@ def checkFileName(fileName):
 
 
 def send_file(file_path, space_id, twitter_name, space_title, space_date):
+    logger = create_logger("logfile_twspace.log")
     if os.path.isfile(file_path):
         webhook = discord.Webhook.from_url(const.WEBHOOK_DOWNLOAD_URL, adapter=discord.RequestsWebhookAdapter())
         space_file = discord.File(file_path)
-        content = f"The twitter space for {twitter_name} was downloaded\n`[{space_date}]{twitter_name} - {space_title}({space_id})`"
+        content = f"The twitter space for {twitter_name} was downloaded\n`[{space_date}] {twitter_name} - {space_title} ({space_id})`"
         try:
             webhook.send(content=content, file=space_file)
         except discord.HTTPException as e:
-            print(f"[error] {e}")
+            # e.status e.text
+            logger.error(e.text)
+            # print(f"[error] {e.text}")
     else:
-        print("[error] Could not find space file to send")
+        logger.error("Could not find space file to send")
+        # print("[error] Could not find space file to send")
 
 
-def download(m3u8_id, space_id, twitter_name, space_title, space_date):
+def download(m3u8_id, space_id, twitter_name, space_title, space_date, periscope_server):
+    logger = create_logger("logfile.log")
     DOWNLOAD_PATH = const.DOWNLOAD
     if DOWNLOAD_PATH == "True":
         DOWNLOAD_PATH = os.getcwd()
     elif not os.path.exists(DOWNLOAD_PATH):
         os.makedirs(DOWNLOAD_PATH)
 
-    base_url = 'https://prod-fastly-us-east-1.video.pscp.tv'
+    base_url = f'https://prod-fastly-{periscope_server}.video.pscp.tv'
     base_addon = '/Transcoding/v1/hls/'
 
     file_name = checkFileName(space_title)
 
     end_masterurl = "/non_transcode/us-east-1/periscope-replay-direct-prod-us-east-1-public/audio-space/master_playlist.m3u8"
-    end_chunkurl = '/non_transcode/ap-northeast-1/periscope-replay-direct-prod-ap-northeast-1-public/audio-space/chunk'
+    # end_chunkurl = '/non_transcode/ap-northeast-1/periscope-replay-direct-prod-ap-northeast-1-public/audio-space/chunk'
+    end_chunkurl = f'/non_transcode/{periscope_server}/periscope-replay-direct-prod-{periscope_server}-public/audio-space/chunk'
     master_url = base_url+base_addon+m3u8_id+end_masterurl
+    # Get the playlist m3u8
     t = urllib.request.urlopen(master_url).read().decode('utf-8')
     master_playlist = re.findall(".*m3u8$", t, re.MULTILINE)
     for i in master_playlist:
         master_playlist = i
 
+    # Get the playlist m3u8 content and replace the chunk url with the appropriate prefix
     chunk_m3u8 = base_url+master_playlist
-
-    t = urllib.request.urlopen(chunk_m3u8).read().decode('utf-8')
+    try:
+        t = urllib.request.urlopen(chunk_m3u8).read().decode('utf-8')
+    except error.HTTPError as httpError:
+        logger.error(httpError)
+        t = urllib.request.urlopen(chunk_m3u8).read().decode('utf-8')
     t = t.replace('chunk', base_url+base_addon+m3u8_id+end_chunkurl)
 
     filename = f'{space_id}.m3u8'
     output = f'{DOWNLOAD_PATH}\\{space_date} - {twitter_name} - {file_name} ({space_id}).aac'
-    command = f'ffmpeg -n -hide_banner -loglevel error -protocol_whitelist file,https,tls,tcp -i {filename} -metadata date=2021 -metadata comment={m3u8_id} -metadata artist={twitter_name} -c copy "{output}"'
+    command = f'ffmpeg -n -hide_banner -loglevel error -protocol_whitelist file,crypto,https,tcp,tls -i {filename} -metadata date=2022 -metadata comment={m3u8_id} -metadata artist={twitter_name} -c copy "{output}"'
 
-    with open(filename, 'x') as f:
+    # Check if the file already exist and if it does remove it
+    if os.path.isfile(filename):
+        os.remove(filename)
+    # Create a new file with the appropriately replaced chunk url
+    with open(filename, 'w') as f:
         f.write(t)
-        os.system(command)
+    os.system(command)
 
     # Only run kid3-cli to set metadata if kid3-cli exist or in path
     if shutil.which("kid3-cli") is not None:
-        #Add metadata using kid3 and metadata
-        #Note metadata won't be shown when using vlc
-        kids3_list = ['kid3-cli', '-c', 'set date "2021"', '-c', f'set artist "{twitter_name}"', '-c', f'set comment "{m3u8_id}"', output]
+        # Add metadata using kid3 and metadata
+        # Note metadata won't be shown when using vlc
+        kids3_list = ['kid3-cli', '-c', 'set date "2022"', '-c', f'set artist "{twitter_name}"', '-c', f'set comment "{m3u8_id}"', output]
         subprocess.run(kids3_list)
+    else:
+        logger.info("kid3-cli can't be found...aborting metadata write...")
+        # print("[info] kid3-cli can't be found...aborting metadata write...")
 
     os.remove(filename)
 
