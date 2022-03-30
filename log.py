5a40bc4140891ec8c7b54b8d761c5e59f65016b1
Sam Phung <sphung@uncc.edu>
2022-03-30 16:01:53 -0400

    Filter and remove stack traceback error message that was printed onto the console

--- log.py
+++ log.py
@@ -10,6 +10,22 @@ class NoParsingFilter(logging.Filter):
         return not record.getMessage().startswith('Sleeping')
 
 
+# Filter subclass to allow and not allow stack traceback filtering
+class TracebackInfoFilter(logging.Filter):
+    """Clear or restore the exception on log records"""
+    def __init__(self, clear=True):
+        self.clear = clear
+    def filter(self, record):
+        if self.clear:
+            record._exc_info_hidden, record.exc_info = record.exc_info, None
+            # clear the exception traceback text cache, if created.
+            record.exc_text = None
+        elif hasattr(record, "_exc_info_hidden"):
+            record.exc_info = record._exc_info_hidden
+            del record._exc_info_hidden
+        return True
+
+
 def create_logger(logfile_name):
     # Check if log dir exist and if not create it
     logging.handlers.TimedRotatingFileHandler
@@ -34,6 +50,8 @@ def create_logger(logfile_name):
     handler.setFormatter(formatter)
     handler.suffix = "%Y%m%d"   # file suffix to be changed
     handler.addFilter(NoParsingFilter())
+    # Set handler to allow stacktraceback logging
+    handler.addFilter(TracebackInfoFilter(clear=False))
 
     # logging.basicConfig(level=logging.INFO,
     #                     format='%(asctime)s [%(filename)s:%(lineno)d] %(levelname)-5s %(message)s',
@@ -43,6 +61,7 @@ def create_logger(logfile_name):
     # define a Handler which writes DEBUG messages or higher to the sys.stderr
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
+    console.addFilter(TracebackInfoFilter())
     # set a format which is simpler for console use
     console_formatter = logging.Formatter('[%(levelname)s] %(message)s')
     # tell the handler to use this format
