dd0ad252f3f978e89612366702ccf1867f7f1e82
Sam Phung <sphung@uncc.edu>
2023-06-13 17:07:42 -0400

    Make changes to use space_creator_name field rather than handle_name

--- twspace.py
+++ twspace.py
@@ -7,6 +7,8 @@ import const
 import discord
 from log import create_logger
 import os
+import re
+import time
 
 
 # Function takes in the file name and check if it contains illegal characters
@@ -65,16 +67,16 @@ def correct_duration(t, duration, logger):
         return False
 
 
-def download(m3u8_id, rest_id, handle_name, space_title, space_server, space_duration, space_date, logger=None):
+def download(m3u8_id, rest_id, space_creator, handle_name, space_title, space_server, space_duration, space_date, logger=None):
     session = requests.Session()
     retry = Retry(total=5, connect=5, backoff_factor=1, status_forcelist=[400, 401, 403, 404, 429, 500, 502, 503, 504])
     session.mount("https://", HTTPAdapter(max_retries=retry))
     if logger is None:
         logger = create_logger("logfile.log")
-    DOWNLOAD_PATH = os.path.join(const.DOWNLOAD, handle_name)
+    DOWNLOAD_PATH = os.path.join(const.DOWNLOAD, space_creator)
     SEND_DOWNLOAD = const.SEND_DOWNLOAD
     if DOWNLOAD_PATH == "True":
-        DOWNLOAD_PATH = os.path.join(os.getcwd(), handle_name)
+        DOWNLOAD_PATH = os.path.join(os.getcwd(), space_creator)
     elif not os.path.exists(DOWNLOAD_PATH):
         os.makedirs(DOWNLOAD_PATH)
 
@@ -126,10 +128,11 @@ def download(m3u8_id, rest_id, handle_name, space_title, space_server, space_dur
     t = t.replace('chunk', base_url+base_addon+m3u8_id+end_chunkurl)
     logger.debug(t)
     filename = f'{rest_id}.m3u8'
-    output = f'{DOWNLOAD_PATH}\\{space_date} - {handle_name} - {file_name} ({rest_id}).m4a'
+    output = f'{DOWNLOAD_PATH}\\{space_date} - {space_creator} - {file_name} ({rest_id}).m4a'
     command = ['ffmpeg', '-n', '-loglevel', 'info', '-protocol_whitelist', 'file,crypto,https,tcp,tls']
-    command += ['-i', filename, '-metadata', f'date={space_date}', '-metadata', f'comment={master_url}']
-    command += ['-metadata', f'artist={handle_name}', '-metadata', f'title={space_title}', '-c', 'copy', output]
+    command += ['-i', filename, '-metadata', f'date={space_date}']
+    command += ['-metadata', f'comment=feat.{handle_name}'] if space_creator != handle_name else ['-metadata', f'comment={master_url}']
+    command += ['-metadata', f'artist={space_creator}', '-metadata', f'title={space_title}', '-c', 'copy', output]
 
     # Check if the file already exist and if it does remove it
     try:
@@ -146,7 +149,7 @@ def download(m3u8_id, rest_id, handle_name, space_title, space_server, space_dur
         logger.debug(download_result.stderr)
 
         if SEND_DOWNLOAD:
-            send_file(output, rest_id, handle_name, space_title, space_date)
+            send_file(output, rest_id, space_creator, space_title, space_date)
         if retry >= MAX_RETRY:
             logger.warning(f"Download completed for {rest_id}, but may not be completely downloaded")
         else:
@@ -164,10 +167,7 @@ def download(m3u8_id, rest_id, handle_name, space_title, space_server, space_dur
 
 
 if __name__ == "__main__":
-    import re
     import threading
-    import time
-    from datetime import datetime
 
     def loading_text():
         loading_string = f"[INFO] Downloading twitter space {space_id} "
@@ -198,13 +198,13 @@ if __name__ == "__main__":
         # space_date = datetime.strptime(space_date, "%Y%m%d").timestamp() * 1000
         m3u8_id = re.search("(.*\/Transcoding\/v1\/hls\/(.*)(\/non_transcode.*))", m3u8_url).group(2)
         server = get_space_server(m3u8_url)
-        # m3u8_id, rest_id, handle_name, space_title, space_server, space_duration, space_date, logger=None
+        # m3u8_id, rest_id, space_creator_name, handle_name, space_title, space_server, space_duration, space_date, logger=None
         t1 = threading.Thread(target=loading_text)
         t1.start()
-        download(m3u8_id=m3u8_id, rest_id=space_id, handle_name=twitter_name, space_title=space_title,
+        download(m3u8_id=m3u8_id, rest_id=space_id, space_creator=twitter_name, handle_name=twitter_name, space_title=space_title,
                  space_server=server, space_duration=None, space_date=space_date)
         status = False
-        input("Download Complete...")
+        input("Download complete, press any key to exit...")
         exit()
     except Exception as e:
         print(f"\rError encountered...{' '*40}\n{e}")
